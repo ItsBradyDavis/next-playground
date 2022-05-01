d19a4c51ff4c3f1d3e43f480c5d3d9602a2dabb1
Brady Davis <bradydavis.ver.2.0@gmail.com>
2022-05-01 16:55:33 -0500

    Lint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: 'get-off-my-lawn',
     rules: {
+        'node/no-sync': 0,
         // enable additional rules, override rule options, or disable rules
     },
 };
