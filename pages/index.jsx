b4130821e8e2b3ef9fae98bd2ee7f40664110079
Brady Davis <bradydavis.ver.2.0@gmail.com>
2022-05-02 16:48:40 -0500

    Adding Dynamic routes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,9 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, {siteTitle} from '../components/Layout';
 import utilStyles from '../styles/utils.module.css';
 import {getSortedPostsData} from '../lib/posts';
+import Date from '../components/date';
 
 const getStaticProps = async () =>{
     const allPostsData = getSortedPostsData();
@@ -19,7 +21,7 @@ const Home = ({allPostsData}) => {
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>Hello! My name is Brady Davis. I've been professionally developing software since 2014, and I am currently a full stack developing consultant at Source Allies. Feel free to contact me on <a href={'https://www.linkedin.com/in/brady-davis-14080589/'}>LinkedIn</a> My areas of expertise include:</p>
+                <p>Hello! My name is Brady Davis. I've been professionally developing software since 2014, and I am currently a full stack developing consultant at Source Allies. Feel free to contact me on <a href={'https://www.linkedin.com/in/brady-davis-14080589/'}>LinkedIn.</a> My areas of expertise include:</p>
                 <ul>
                     <li>Frontend & Backend development using NodeJs stack, including Next, React, React Native, and GraphQl</li>
                     <li>Flash AS3 Web & Mobile application development</li>
@@ -28,21 +30,20 @@ const Home = ({allPostsData}) => {
                     <li>Vb.NET and C# .NET application development.</li>
                 </ul>
                 <p></p>
-                <p>
-                    (This is a sample website - you’ll be building a site like this on{' '}
-                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-                </p>
+
             </section>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
                     {allPostsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
-                            {title}
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
                             <br />
-                            {id}
-                            <br />
-                            {date}
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
                         </li>
                     ))}
                 </ul>
