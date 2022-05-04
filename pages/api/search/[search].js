671448d1639c065ab0b26208cfc8bf83b86ae89f
Brady Davis <bradydavis.ver.2.0@gmail.com>
2022-05-03 21:24:36 -0500

    Adding search endpoint

--- pages/api/search/[search].js
+++ pages/api/search/[search].js
@@ -1,9 +1,17 @@
-const handler = (req, res) => {
+import {getGifsForSearchQuery} from '../../../repositories/gif-repository';
+
+const handler = async (req, res) => {
     const {
         query: {search}
     } = req;
 
+    const gifResults = await getGifsForSearchQuery(search);
 
+    if (gifResults) {
+        res.status(200).json(gifResults);
+    } else {
+        res.status(500);
+    }
 };
 
 export default handler;
