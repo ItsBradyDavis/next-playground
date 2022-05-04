b9ec691334f9d60135c5af427a2ebdbb9afbe24b
Brady Davis <bradydavis.ver.2.0@gmail.com>
2022-05-03 19:36:33 -0500

    Fixing lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
     extends: 'get-off-my-lawn',
+    ignorePatterns: [
+        '/coverage/*',
+        'jest.config.js',
+    ],
     rules: {
         'node/no-sync': 0,
         // enable additional rules, override rule options, or disable rules
--- test/pages/api/health.spec.js
+++ test/pages/api/health.spec.js
@@ -1,26 +1,29 @@
 import handler from '../../../pages/api/health';
 
 describe('health', () => {
-    let req, res, status, json;
+    let req,
+        res,
+        status,
+        json;
 
     beforeEach(() => {
         json = jest.fn();
         status = jest.fn().mockReturnValue({json});
         res = {
-            status
+            status,
         };
         req = {};
     });
 
     test('should return status 200 & ok status object', () => {
-       handler(req, res);
+        handler(req, res);
 
-       expect(status).toHaveBeenCalledTimes(1);
-       expect(status).toHaveBeenCalledWith(200);
+        expect(status).toHaveBeenCalledTimes(1);
+        expect(status).toHaveBeenCalledWith(200);
 
-       expect(json).toHaveBeenCalledTimes(1);
-       expect(json).toHaveBeenCalledWith({
-           status: 'ok'
-       })
+        expect(json).toHaveBeenCalledTimes(1);
+        expect(json).toHaveBeenCalledWith({
+            status: 'ok',
+        });
     });
 });
